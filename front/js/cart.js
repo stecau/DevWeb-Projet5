43fe1de2ef1e4b7f5242f93cf2531a4ce1883a64
Stephane CAU <stephane-jean-noel.cau@orange.fr>
2022-07-19 23:58:47 +0200

    Creation fo the function in order to update the 'cart' page at its initialisation

---
+++ front/js/cart.js
@@ -0,0 +1,103 @@
+/*--------------------------------------------------------------------------------------*/
+/* Functions for getting information from API directly or from 'archived' API responses */
+
+/* Function to get the products object based on its 'id' from API */
+const getAPIElementKanap = (productID) => fetch("http://localhost:3000/api/products/" + productID)
+    .then(function(res) {
+        if (res.ok) {
+            return res.json();
+        }
+    })
+    .then(function(value) {
+        return value;
+    })
+    .catch(function(err) {
+        // Une erreur est survenue
+        console.log("ERREUR return data from 'getAPIElementKanap(productID)': ");
+        console.log(err);
+        return 102;
+    });
+
+/*--------------------------------------------------------------------------------------*/
+
+
+/*--------------------------------------------------------------------------------------*/
+/* Functions for page 'Cart' and its dynamical modification */
+/* Get list of article from LocalStorage */
+const updateCartPage = async (localStorage) => {
+    const listOfArticleObjectFromAPI = [];
+    for (let index = 0; index < localStorage.length; index++) {
+        const localStorageObject = JSON.parse(localStorage.getItem(localStorage.key(index)));
+        // Get article information from API with its ID and store data in list
+        listOfArticleObjectFromAPI.push({
+            "article": await getAPIElementKanap(localStorageObject.id),
+            "color": localStorageObject.color,
+            "quantite": localStorageObject.quantity
+        });
+    }
+    // Modifiy innertHTML of the 'cart__items' section
+    const parentContainer = document.getElementById("cart__items");
+    parentContainer.innerHTML = ecritureInnerHTML(listOfArticleObjectFromAPI);
+    // Modifiy textcontent of the 'cart__price' div
+    const total = calculTotalArticleAndPrice(listOfArticleObjectFromAPI)
+    document.getElementById("totalQuantity").textContent = total.totalArticle;
+    document.getElementById("totalPrice").textContent = total.totalPrice;
+}
+
+/* Sub Function in order to write the innerHTML text */
+const ecritureInnerHTML = (listElements) => {
+    let texte = ``;
+    for (index in listElements) {
+        texte += 
+`<article class="cart__item" data-id="${listElements[index].article._id}" data-color="${listElements[index].color}">
+    <div class="cart__item__img">
+        <img src="${listElements[index].article.imageUrl}" alt="${listElements[index].article.altTxt}">
+    </div>
+    <div class="cart__item__content">
+        <div class="cart__item__content__description">
+            <h2>${listElements[index].article.name}</h2>
+            <p>${listElements[index].color}</p>
+            <p>${listElements[index].article.price} €</p>
+        </div>
+        <div class="cart__item__content__settings">
+            <div class="cart__item__content__settings__quantity">
+                <p>Qté : </p>
+                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${listElements[index].quantite}">
+            </div>
+            <div class="cart__item__content__settings__delete">
+                <p class="deleteItem">Supprimer</p>
+            </div>
+        </div>
+    </div>
+</article>`;
+    }
+    return texte;
+};
+
+/* Sub Function in order to get the total of articles and the total price */
+const calculTotalArticleAndPrice = (listElements) => {
+    let totalArticle = 0;
+    let totalPrice = 0;
+    for (index in listElements) {
+        totalArticle += listElements[index].quantite;
+        totalPrice += listElements[index].quantite * listElements[index].article.price;
+    }
+    return {"totalArticle": totalArticle, "totalPrice": totalPrice};
+};
+
+
+/*--------------------------------------------------------------------------------------*/
+/* Function MAIN of the web site */
+const main = async () => {
+    // Update 'cart' page with LocalStorage articles
+    const setKanapCartInformation = updateCartPage(window.localStorage);
+
+    
+
+};
+
+/*--------------------------------------------------------------------------------------*/
+/* Lancement script with main function */
+console.log("Script final de la page product : ne doit pas contenir de message 'log' ou 'ERREUR'");
+main()
+/*--------------------------------------------------------------------------------------*/
